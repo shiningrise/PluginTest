2acf517c5a1eb690c1000458b8c8bc6cab4e992e
wxy <shiningrise@gmail.com>
2014-11-28 14:23:49 +0800

    Init

---
+++ PluginTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PluginTest.MvcApplication" Language="C#" %>
---
+++ PluginTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PluginTest.Startup))]
+namespace PluginTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
